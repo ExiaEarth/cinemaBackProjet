4b6e15a5273ce9877d12d6946e7d0e802eab93c4
Marclemer <marc-lemer@hotmail.com>
2022-09-12 11:54:53 +0200

    User-dto fin

---
+++ Dto/user-dto.js
@@ -0,0 +1,11 @@
+class UserDTO{
+    constructor(id,pseudo,prenom,nom,email,){
+        this.id=id;
+        this.pseudo=pseudo;
+        this.prenom=prenom;
+        this.nom=nom;
+        this.email=email;
+    }
+}
+
+module.exports=UserDTO
