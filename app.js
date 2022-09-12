ec9cdd08e929a6045e579f7162611d9308c58c3a
Marclemer <marc-lemer@hotmail.com>
2022-09-12 11:13:18 +0200

    mise en place su serveur réussi

---
+++ app.js
@@ -0,0 +1,22 @@
+require('dotenv-flow').config();
+const express=require('express');
+const cors=require('cors');
+const{DB_CONNECTION,PORT}=process.env;
+
+console.log('Je suis lancée sur le',PORT);
+require('express-async-errors');
+const mongoose=require('mongoose');
+const app=express();
+app.use(cors());
+
+// const router=require('./Routes');
+
+app.use(async(req,res,next)=>{await mongoose.connect(DB_CONNECTION)
+    console.log('connection réussie !!!!!!')
+    next();
+});
+
+app.use(express.json());
+// app.use('/api',router);
+app.listen(PORT,()=>{console.log(`Le serveur est sur le port: ${PORT}`);
+});
