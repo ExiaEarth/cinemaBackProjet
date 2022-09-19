9911287e16b6a4c74cdf3285045979fdb5851c08
Marclemer <marc-lemer@hotmail.com>
2022-09-19 22:19:19 +0200

    maj 19/09/22

--- Routes/user-route.js
+++ Routes/user-route.js
@@ -4,11 +4,25 @@ const idValidateur = require("../Middlewares/id-middlewares");
 const userValidateur=require('../Validateur/user-validateur');
 
 const userRouter=require('express').Router();
+const multer=require('multer');
+const uuid=require('uuid');
+const identificationController = require("../Controllers/identification-controller");
 
+const storage=multer.diskStorage({
+    destination:(req,file,cb)=>{cb(null,'putblic/Avatars')},
+
+    filename:(req,file,cb)=>{
+        const name=uuid.v4();
+        const extension=file.originalname.split('.').at(-1);
+        cb(null,name+'.'+extension)
+    }
+});
+const upload=multer({storage});
 
 
 userRouter.route('/')
     .get(userController.getAll);
+    .post(upload.single('avatar'),identificationController.register)
 userRouter.route('/:id')
     .get(idValidateur(),userController.getById)
     .put(idValidateur(),bodyValidation(userValidateur),userController.update)
